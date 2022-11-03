d19b6f1c62ae3dbae36fe96c3221a7fa6b263dcf
Raihan Aria <raihan.aria.ft21@mail.umy.ac.id>
2022-11-03 15:41:05 +0700

    create a method to enter numbers

--- exercise2/Program.cs
+++ exercise2/Program.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace exercise2
 {
-    internal class Program
-    {
+    class Program
+    { private int[] a = new int[27];
+    private int n;
+
+        public void read()
+        {
+            while (true)
+            {
+                Console.WriteLine("Masukkan banyak elemen pada array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 27)
+                    break;
+                else
+                    Console.WriteLine("\nArray maksimal 20 elemen.\n");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Masukkan Element Array");
+            Console.WriteLine("----------------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                a[i] = Int32.Parse(s1);
+            }
+        }
         static void Main(string[] args)
         {
         }
